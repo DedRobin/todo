1371566caf31da110db72316d818a0bf84d68ed2
Pavel Rabekin <pavelrabekin@gmail.com>
2022-08-05 21:34:40 +0300

    update base.html, note.html

--- tests.py
+++ tests.py
@@ -1,34 +1,21 @@
-import json
-import requests
+import os
+import django
 
-from faker import Faker
-
-fake = Faker()
-
+from django.test.client import Client
 
 if __name__ == "__main__":
-    response = requests.get("http://127.0.0.1:5000/")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
+    django.setup()
+
+    client = Client()
+
+    base_url = "http://localhost:8000"
+    response = client.get(base_url)
     assert response.status_code == 200
-    assert response.json() == {"message": "Index page"}
-    assert response.headers.get("content-type") == "application/json"
 
-    headers = {"Content-Type": "application/json"}
-    data = {"email": "", "password": ""}
-    response = requests.post("http://127.0.0.1:5000/user/", data=json.dumps(data), headers=headers)
-    assert response.status_code == 400
+    client.login(username="manti", password="Dalt0nik")
 
-    headers = {"Content-Type": "application/json"}
-    data = {"email": fake.email(), "password": fake.word()}
-    response = requests.post("http://127.0.0.1:5000/user/", data=json.dumps(data), headers=headers)
-
-    assert response.status_code == 201
-    assert response.json().get("email") == data.get("email")
-    assert response.json().get("id") is not None
-    assert response.headers.get("content-type") == "application/json"
-
-    response = requests.get("http://127.0.0.1:5000/user/")
+    response = client.post(base_url, data={"title": "test", "text": "test"}, follow=True)
     assert response.status_code == 200
-    assert len(response.json().get("users")) > 0
-    assert response.headers.get("content-type") == "application/json"
 
     print("All tests have passed")
