2b1d8c82737a0583d53cabe89f43ec3931b51d3f
Pavel Rabekin <pavelrabekin@gmail.com>
2022-08-19 12:15:27 +0300

    added test for registrtion (it does't work)

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,6 +1,7 @@
 import pytest
 
 from django.test.client import Client
+from django.contrib.auth.models import User
 
 
 @pytest.mark.django_db
@@ -12,3 +13,18 @@ class TestViews:
     def test_index(self):
         response = self.client.get("/")
         assert response.status_code == 200
+
+    @pytest.mark.skip
+    def test_register_login_logout(self):
+        response = self.client.get("/register")
+        assert response.status_code == 200
+
+        self.client.post('/register/', {
+            "username": "test_user",
+            "email": "test_email",
+            "first_name": "test_first_name",
+            "last_name": "test_first_name",
+            "password": "test_password123!"
+        })
+        is_user_existed = User.objects.filter(username='test_user').exists()
+        assert is_user_existed == True
