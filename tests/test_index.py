ab18e5c02f50a6b735a1367d69cd28e1ba3b43cf
Pavel Rabekin <pavelrabekin@gmail.com>
2022-08-19 18:15:33 +0300

    some fix

--- tests/test_index.py
+++ tests/test_index.py
@@ -14,17 +14,17 @@ class TestViews:
         response = self.client.get("/")
         assert response.status_code == 200
 
-    @pytest.mark.skip
-    def test_register_login_logout(self):
-        response = self.client.get("/register")
-        assert response.status_code == 200
-
-        self.client.post('/register/', {
-            "username": "test_user",
-            "email": "test_email",
-            "first_name": "test_first_name",
-            "last_name": "test_first_name",
-            "password": "test_password123!"
-        })
-        is_user_existed = User.objects.filter(username='test_user').exists()
-        assert is_user_existed == True
+    # @pytest.mark.skip
+    # def test_register_login_logout(self):
+    #     response = self.client.get("/register")
+    #     assert response.status_code == 200
+    #
+    #     self.client.post('/register/', {
+    #         "username": "test_user",
+    #         "email": "test_email",
+    #         "first_name": "test_first_name",
+    #         "last_name": "test_first_name",
+    #         "password": "test_password123!"
+    #     })
+    #     is_user_existed = User.objects.filter(username='test_user').exists()
+    #     assert is_user_existed == True
